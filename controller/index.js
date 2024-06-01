517c4a98f85926b52a827b2d1eb3ade1a13c93ac
scapula07 <bartholomewonogwu@yahoo.com>
2024-06-01 13:29:19 +0100

    pushing noones api

--- controller/index.js
+++ controller/index.js
@@ -53,6 +53,10 @@ exports.recentTrades= async (req, res, next) => {
         })
         .catch(error => {
             console.error('Error:', error);
+            res.status(400).json({
+              status: 'error',
+              data:error?.data
+           });
         });
 
 
@@ -91,6 +95,10 @@ exports.recentTrades= async (req, res, next) => {
  })
  .catch(error => {
      console.error('Error:', error);
+     res.status(401).json({
+      status: 'error',
+      data:error?.data
+   });
  });
 
 
@@ -121,7 +129,7 @@ exports.getTrade= async (req, res, next) => {
      console.log('Access Token:', response.data);
      res.status(200).json({
        status: 'success',
-       data:response.data?.data?.trades
+       data:response.data?.data
    });
  })
  .catch(error => {
@@ -147,7 +155,7 @@ exports.release= async (req, res, next) => {
   const {token,hash} = req.body
   console.log(req.body,"ttttt")
  try{
-   axios.post('https://api.noones.com/noones/v1/trade/list',{}, {
+   axios.post('https://api.noones.com/noones/v1/trade/release',{trade_hash:hash}, {
      headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization' : `Bearer ${token} `
@@ -298,6 +306,40 @@ exports.getImage= async (req, res, next) => {
   console.log(req.body,"ttttt")
  try{
    axios.post('https://api.noones.com/noones/v1/trade-chat/post',{message:msg,trade_hash:hash}, {
+     headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization' : `Bearer ${token} `
+          }
+      })
+ .then(response => {
+     console.log('Access Token:', response.data);
+     res.status(200).json({
+       status: 'success',
+       data:response.data
+   });
+ })
+ .catch(error => {
+     console.error('Error:', error);
+ });
+
+
+
+
+
+
+}catch(e){
+   console.log(e)
+}
+
+}
+
+
+
+exports.getUser= async (req, res, next) => {
+  const {token} = req.body
+  console.log(req.body,"ttttt")
+ try{
+   axios.post('https://api.noones.com/noones/v1/user/wallet-balances',{}, {
      headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization' : `Bearer ${token} `
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getAccessToken,recentTrades,getTrade,getChat,sendChat}=require("../controller/index")
+const {getAccessToken,recentTrades,getTrade,getChat,sendChat,release,getUser}=require("../controller/index")
 
 router.route('/get-token').get(getAccessToken);
 router.route('/get-trades').post(recentTrades);
@@ -10,6 +10,9 @@ router.route('/get-trade').post(getTrade);
 
 router.route('/get-chat').post(getChat);
 router.route('/send-chat').post(sendChat);
+router.route('/release').post(release);
+
+router.route('/user').post(getUser);
 
 
 
